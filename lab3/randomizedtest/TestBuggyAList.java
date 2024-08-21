933ee3fe0021b53f1e07348310d6de5b6f593428
RingPhone666 <610828739@qq.com>
2024-08-21 23:17:34 +0800

    lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -11,20 +11,53 @@ public class TestBuggyAList {
   // YOUR TESTS HERE
     @Test
     public void testbuggy1(){
-        AListNoResizing<Integer> L = new AListNoResizing<>();
-
-        int N = 500;
+        /* <Integer> correctAList=new AListNoResizing<>() ;
+        BuggyAList <Integer> wrongAlist =new BuggyAList<>();
+        int a=1,b=2,c=3;
+        correctAList.addLast(a);
+        wrongAlist.addLast(a);
+        correctAList.addLast(b);
+        correctAList.addLast(c);
+        wrongAlist.addLast(b);
+        wrongAlist.addLast(c);
+        assertEquals(correctAList.size(),wrongAlist.size());
+        assertEquals(correctAList.removeLast(),wrongAlist.removeLast());*/
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+        int N = 10;
+        for (int i = 0; i < 10; i++) {
+            int randVal = StdRandom.uniform(0, 100);
+            correct.addLast(randVal);
+            broken.addLast(randVal);
+        }
+        assertEquals(correct.size(), broken.size());
+        for (int i = 0; i < N; i++) {
+            assertEquals(correct.removeLast(), broken.removeLast());
+        }
+    }
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 3);
+            int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
-                L.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
+                correct.addLast(randVal);
+                broken.addLast(randVal);
             } else if (operationNumber == 1) {
                 // size
-                int size = L.size();
-                System.out.println("size: " + size);
+                assertEquals(correct.size(), broken.size());
+            } else if (correct.size() == 0) {
+                continue;
+            } else if (operationNumber == 2) {
+                // getLast
+                assertEquals(correct.getLast(), broken.getLast());
+            } else if (operationNumber == 3) {
+                // removeLast
+                assertEquals(correct.removeLast(), broken.removeLast());
             }
         }
     }
